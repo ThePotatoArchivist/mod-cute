9ee4589f4610148735ec1c6dde13e451966eb2ff
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-13 01:21:38 -0700

    more work on filter controls

--- src/FilterControls.svelte
+++ src/FilterControls.svelte
@@ -1,10 +1,61 @@
 <script lang="ts">
-    import type { TAGS } from './modrinth';
+    import type { TagTypes } from './modrinth';
     import type { Facets } from './lib/modrinth/facets';
+    import type { Category, GameVersion, Loader } from '@xmcl/modrinth';
+    import ButtonGroup from './lib/component/ButtonGroup.svelte';
 
-    export let value: Facets = []
-    export let tags: Awaited<typeof TAGS>
+    export let facets: Facets = []
+    export let tags: TagTypes
+    const { categories, loaders, projectTypes, sideTypes, versions } = tags
+    
+    let selectedProjectType: string | undefined
+    let selectedCategory: Category
+    let selectedLoader: Loader
+    let serverSide: string
+    let clientSide: string
+    let selectedVersion: GameVersion
+    
+    $: relevantCategories = selectedProjectType === undefined ? [] : categories.filter(e => e.project_type === selectedProjectType)
+    $: relevantLoaders = selectedProjectType === undefined ? [] : loaders.filter(e => e.supported_project_types.includes(selectedProjectType!))
+    let allVersions = false
+    $: relevantVersions = allVersions ? versions : versions.filter(e => e.version_type === 'release')
 </script>
 
-{JSON.stringify(tags.categories)}
+<div>
+    <ButtonGroup options={projectTypes} bind:value={selectedProjectType} />
+</div>
 
+{#if selectedProjectType !== undefined}
+    <div>
+        <ButtonGroup options={relevantCategories} bind:value={selectedCategory} let:option>
+            {option.name}
+        </ButtonGroup>
+    </div>
+    
+    {#if relevantLoaders.length > 1}
+        <div>
+            <ButtonGroup options={relevantLoaders} bind:value={selectedLoader} let:option>
+                {option.name}
+            </ButtonGroup>
+        </div>
+    {/if}
+    
+    <div>
+        <ButtonGroup options={sideTypes} bind:value={serverSide} />
+    </div>
+
+    <div>
+        <ButtonGroup options={sideTypes} bind:value={clientSide} />
+    </div>
+    
+    <div>
+        <label>
+            <input type="checkbox" bind:checked={allVersions} />
+            Show all versions
+        </label>
+
+        <ButtonGroup options={relevantVersions} bind:value={selectedVersion} let:option>
+            {option.version}
+        </ButtonGroup>
+    </div>
+{/if}
---
+++ src/lib/component/ButtonGroup.svelte
@@ -0,0 +1,13 @@
+<script lang="ts" generics="T">
+    export let options: T[]
+    export let value: NoInfer<T> | undefined
+</script>
+
+{#each options as option}
+    <label>
+        <input type="radio" bind:group={value} value={option} />
+        <slot {option}>
+            {option}
+        </slot>
+    </label>
+{/each}
